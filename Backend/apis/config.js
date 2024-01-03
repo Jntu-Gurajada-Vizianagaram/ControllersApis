8479e2a76667d62be8e06c5ce7bf1ced8e0807cb
dsak789 <dsak.official@gmail.com>
2024-01-03 23:57:05 +0530

    Admin Console API INTEGRATIONS

--- Backend/apis/config.js
+++ Backend/apis/config.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql2");
 
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1437890",
-  database: "jntugv",
-  port: "3306"  ,
-});
 // const con = mysql.createConnection({
-//   host: "3.7.40.139",
-//   user: "JNTUGV",
-//   password: "password",
+//   host: "localhost",
+//   user: "root",
+//   password: "1437890",
 //   database: "jntugv",
-//   port: "3306",
+//   port: "3306"  ,
 // });
+const con = mysql.createConnection({
+  host: "3.7.40.139",
+  user: "JNTUGV",
+  password: "password",
+  database: "jntugv",
+  port: "3306",
+});
 // const con=mysql.createConnection({
 //     host:"localhost",
 //     user:"root",
