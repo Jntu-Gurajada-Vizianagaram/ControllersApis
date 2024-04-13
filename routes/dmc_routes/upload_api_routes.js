e1aabc24f13bf1426d6e075a407d64e21d687da8
dsak789 <dsak.official@gmail.com>
2024-04-14 00:38:18 +0530

    Event Photos API Added

---
+++ routes/dmc_routes/upload_api_routes.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router();
+const webadmin= require('../../apis/dmc_api/DMCApi')
+
+
+
+
+//------APIS for admin console-------//
+
+router.get('/allimages',webadmin.all_imgs); 
+router.post('/addimage',webadmin.dmcUpload,webadmin.insert_img)
+router.get('/removeimg/:id',webadmin.delete_img);
+router.get('/carousel-images',webadmin.carousel_imgs);
+// router.patch('/update-event',router.update_event)
+
+//------API for BULK Images ----//
+router.post('/add-event-photos',webadmin.bulkupload,webadmin.add_event_photos)
+router.get('/get-event-photos',webadmin.get_events_photos)
+//------Event Photos---Request APIS---//
+router.get('/webadmin-event-requests',webadmin.webadmin_event_requests); //api for only admin webadmin events added by webadminr
+router.get('/webadmin-event-accept-request/:id',webadmin.webadmin_event_request_accept); //api for only admin to accept events added by webadminr
+router.get('/webadmin-event-deny-request/:id',webadmin.webadmin_event_request_deny); //api for only admin to deny events added by webadminr
+
+
+//---Request APIS---//
+router.get('/webadmin-requests',webadmin.webadmin_requests); //api for only admin webadmin events added by webadminr
+router.get('/webadmin-accept-request/:id',webadmin.webadmin_request_accept); //api for only admin to accept events added by webadminr
+router.get('/webadmin-deny-request/:id',webadmin.webadmin_request_deny); //api for only admin to deny events added by webadminr
+
+
+// ----- Apis for Frontend----------//
+router.get('/main-carousel-images',webadmin.carousel_imgs);
+router.get('/get-main-event-photos',webadmin.get_main_events_photos)
+
+
+
+module.exports=router
