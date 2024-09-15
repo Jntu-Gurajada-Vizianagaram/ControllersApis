44da7853ad7bb34dad3181648fdd0a133e5b1b31
anilsinthu73 <21vv5a1273@jntugvcev.in>
2024-09-15 10:30:49 +0530

    ..

--- Schemas/galleryRequestsSchema.js
+++ Schemas/galleryRequestsSchema.js
@@ -1,6 +1,32 @@
 const con = require('../apis/config.js');
 
-exports.deleteGalleryImages = () => {
+exports.gallery_requests = () => {
+  try {
+        // Create the galleryimages table if it doesn't exist
+    const createGalleryImagesTable = `
+      CREATE TABLE IF NOT EXISTS galleryimages (
+        id SERIAL PRIMARY KEY,                          -- Auto-incrementing unique identifier
+        imagelink VARCHAR(255) NOT NULL,                -- Link to access the image, usually a URL
+        description TEXT,                               -- Description of the image
+        uploaded_date DATE NOT NULL,                    -- Date the image was uploaded
+        event_name VARCHAR(255),                        -- Name of the event associated with the image
+        added_by VARCHAR(100)                           -- Username or identifier of the person who uploaded the image
+      );
+    `;
+
+    // Execute the CREATE TABLE query
+    con.query(createGalleryImagesTable, (err, result) => {
+      if (err) {
+        console.error('Error creating  table:', err);
+      } else {
+        // console.log('');
+      }
+    });
+
+  } catch (err) {
+    console.error('Error in table creation:', err);
+  }
+
   try {
     const deleteImagesQuery = `
       DELETE FROM galleryimages
@@ -19,4 +45,5 @@ exports.deleteGalleryImages = () => {
   } catch (err) {
     console.error('Error in deletion process:', err);
   }
+
 };
